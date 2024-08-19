40642a9daf6a16466ba9ad8c9547d1f83a59f3a5
jdmax15 <joel.maxwell15@gmail.com>
2024-08-19 14:22:08 +0930

    added operational appending to self.history

--- calculator.py
+++ calculator.py
@@ -7,21 +7,36 @@ class Calculator:
         self.history_limit = history_limit
         self.operations = ['add', 'subtract', 'multiply', 'divide']
 
-    def add(x, y):
-        return x + y
+    def add(self, x, y):
+        answer = x + y
+        self.history.append(f"{x} + {y} = {answer}")
+        return answer
     
-    def subtract(x, y):
-        return x - y
+    def subtract(self, x, y):
+        answer = x - y
+        self.history.append(f"{x} - {y} = {answer}")
+        return answer
     
-    def multiply(x, y):
-        return x * y
+    def multiply(self, x, y):
+        answer = x * y
+        self.history.append(f"{x} * {y} = {answer}")
+        return answer
     
-    def divide(x, y):
-        return x / y
+    def divide(self, x, y):
+        answer = x / y
+        self.history.append(f"{x} / {y} = {answer}")
+        return answer
+    
+    def get_history(self):
+        for line in self.history:
+            print(line)
     
 
-calc = Calculator
+calc = Calculator(10)
 
-result = calc.add(2, 3)
-
-print(result)
+print(calc.add(2, 3))
+print(calc.subtract(3, 2))
+print(calc.multiply(2, 3))
+print(int(calc.divide(4, 2)))
+calc.get_history()
+#print(calc.history_limit)
